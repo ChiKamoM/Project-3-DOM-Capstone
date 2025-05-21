b1ab7dfd55b2f458115d238664a0697c0dab7b6f
Chinasa <kamogelomba@gmail.com>
2025-05-21 14:55:18 +0200

    last change

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 'use strict';
 for (var i = 0; i < document.querySelectorAll(".show-modal").length; i++) {
-    document.querySelector(".show-modal").addEventListener("click",function () {
-     alert("clicked") 
+    document.querySelectorAll(".show-modal")[i].addEventListener("click",function () {
+     document.querySelector(".modal").classList.remove("hidden")
+     document.querySelector(".close-modal").addEventListener("click", function () {
+        document.querySelector(".modal").classList.add("hidden")
+     })
     })  
 }
