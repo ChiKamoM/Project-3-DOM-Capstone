834cbd1099ddde69031cb01bbdf24a2a4066299d
Chinasa <kamogelomba@gmail.com>
2025-05-21 14:48:58 +0200

    latest changes

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+for (var i = 0; i < document.querySelectorAll(".show-modal").length; i++) {
+    document.querySelector(".show-modal").addEventListener("click",function () {
+     alert("clicked") 
+    })  
+}
